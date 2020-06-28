7d85595e1fb4c5968bf4c358b4fbb702b26c7755
Joshua <Jplittle2001@gmail.com>
2020-06-28 15:31:30 -0400

    Updates

---
+++ precache-manifest.62ad7e2641c46cebfae350489fc224f8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b659f9d204bf88d418f0e39163873a15",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "507c31e759e6286bfa41",
+    "url": "/react-chess/static/css/main.911359a6.chunk.css"
+  },
+  {
+    "revision": "34d2e656c451c582b27a",
+    "url": "/react-chess/static/js/2.68563be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-chess/static/js/2.68563be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507c31e759e6286bfa41",
+    "url": "/react-chess/static/js/main.8f229fad.chunk.js"
+  },
+  {
+    "revision": "b202356f4e218998bc4c",
+    "url": "/react-chess/static/js/runtime-main.421fb380.js"
+  },
+  {
+    "revision": "cc44037117954ff24ba852067e60a58a",
+    "url": "/react-chess/static/media/b_bishop.cc440371.svg"
+  },
+  {
+    "revision": "f0d0d19f08b6d17a04a7c46df176dcd1",
+    "url": "/react-chess/static/media/b_king.f0d0d19f.svg"
+  },
+  {
+    "revision": "a651e3ac0af3188f884411c0532a223b",
+    "url": "/react-chess/static/media/b_knight.a651e3ac.svg"
+  },
+  {
+    "revision": "c64efe837a867aed78339e618a779ec7",
+    "url": "/react-chess/static/media/b_pawn.c64efe83.svg"
+  },
+  {
+    "revision": "21b99ed20946db159583ddb98d10685c",
+    "url": "/react-chess/static/media/b_queen.21b99ed2.svg"
+  },
+  {
+    "revision": "f4020d2d622bb5e14ccfb70c3c944226",
+    "url": "/react-chess/static/media/b_rook.f4020d2d.svg"
+  },
+  {
+    "revision": "c3b7f72a8ac62ba48116a1e73531a356",
+    "url": "/react-chess/static/media/board.c3b7f72a.svg"
+  },
+  {
+    "revision": "a258f388a4e655e38db19a65d98a9b60",
+    "url": "/react-chess/static/media/profile.a258f388.svg"
+  },
+  {
+    "revision": "07e16392d98d0e9d575f19f807a7c430",
+    "url": "/react-chess/static/media/w_bishop.07e16392.svg"
+  },
+  {
+    "revision": "209f8af07a1c55f5d0c5decda4b04ce9",
+    "url": "/react-chess/static/media/w_king.209f8af0.svg"
+  },
+  {
+    "revision": "a06a905631d11da8513065ba48d63b33",
+    "url": "/react-chess/static/media/w_knight.a06a9056.svg"
+  },
+  {
+    "revision": "5cf462e46b211ceaab2ae0c9c0c2a53c",
+    "url": "/react-chess/static/media/w_pawn.5cf462e4.svg"
+  },
+  {
+    "revision": "47bf56f2fa46f90ee63ddaa5125a49e4",
+    "url": "/react-chess/static/media/w_queen.47bf56f2.svg"
+  },
+  {
+    "revision": "a3e85776ba89b219453e908d46ad03ed",
+    "url": "/react-chess/static/media/w_rook.a3e85776.svg"
+  }
+]);
